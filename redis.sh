158ae4ff9effa2cdefca009e718577d78f3615f4
ToorubatlaUpendra <upendratoorubatla@gmail.com>
2024-04-11 15:22:27 +0530

    catalouge

---
+++ redis.sh
@@ -0,0 +1,53 @@
+#!/bin/bash
+
+ID=$(id -u)
+R="\[31m"
+G="\[32m"
+Y="\[33m"
+N="\[0m"
+TIMESTAMP=$(date +%F-%H-%M-%S)
+LOGFILE="/tmp/$0-$TIMESTAMP.log"
+VALIDATE(){
+    if [ $1 -ne 0 ]
+    then
+        echo -e "ERROR:: $2 ... $R FAILED $N"
+        exit 1
+    else
+        echo -e "$2 ... $G SUCCESS $N"
+    fi
+}
+echo "script started executing at $TIMESTAMP" &>> $LOGFILE
+if [ $ID -ne 0 ]
+then
+    echo -e "$R ERROR:: Please run this script with root access $N"
+    exit 1 # you can give other than 0
+else
+    echo "You are root user"
+fi # fi means reverse of if, indicating condition end
+
+dnf install https://rpms.remirepo.net/enterprise/remi-release-8.rpm -y &>> $LOGFILE
+
+VALIDATE $? "downloading remi" 
+
+dnf module enable redis:remi-6.2 -y &>> $LOGFILE
+
+VALIDATE $? "enablining redis" 
+
+dnf install redis -y &>> $LOGFILE
+
+VALIDATE $? "installing redis" 
+
+sed -i 's/127.0.0.1/0.0.0.0' /etc/redis.conf  &>> $LOGFILE
+
+VALIDATE $? "Replacing with public ip" 
+
+systemctl enable redis &>> $LOGFILE
+
+VALIDATE $? "enabling redis" 
+
+
+systemctl start redis &>> $LOGFILE 
+
+VALIDATE $? "starting redis" 
+
+
