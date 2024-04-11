8f098629cb2569a9dafe5762fc2f42879b3d4dc7
ToorubatlaUpendra <upendratoorubatla@gmail.com>
2024-04-11 14:57:07 +0530

    catalouge

--- catalouge.sh
+++ catalouge.sh
@@ -72,7 +72,7 @@ npm install &>>$LOGFILE
 
 VALIDATE $? "npm install"
 
-cp catalouge.service /etc/systemd/system/catalogue.service &>>$LOGFILE
+cp /C:\Users\tooru\testforgit\roboshop-shell\catalouge.service /etc/systemd/system/catalogue.service &>>$LOGFILE
 
 VALIDATE $? "copying service file"
 
