c84a61123b05f7332397f284a27234076d3c6a55
ToorubatlaUpendra <upendratoorubatla@gmail.com>
2024-04-11 16:19:07 +0530

    catalouge

---
+++ mysql.sh
@@ -0,0 +1,39 @@
+#!/bin/bash 
+ID=$(id -u)
+R="\[31m"
+G="\[32m"
+Y="\[33m"
+N="\[0m"
+TIMESTAMP=$(date +%F-%H-%M-%S)
+LOGFILE="/tmp/$0-$TIMESTAMP.log"
+VALIDATE(){
+    if [ $1 -ne 0 ]
+    then
+        echo -e "ERROR:: $2 ... $R FAILED $N"
+        exit 1
+    else
+        echo -e "$2 ... $G SUCCESS $N"
+    fi
+}
+echo "script started executing at $TIMESTAMP" >> $LOGFILE
+if [ $ID -ne 0 ]
+then
+    echo -e "$R ERROR:: Please run this script with root access $N"
+    exit 1 # you can give other than 0
+else
+    echo "You are root user"
+fi # fi means reverse of if, indicating condition end
+
+dnf module disable mysql -y
+
+cp /root/roboshop-shell/mysql.repo /etc/yum.repos.d/mysql.repo
+
+dnf install mysql-community-server -y
+
+systemctl enable mysqld
+
+systemctl start mysqld
+
+mysql_secure_installation --set-root-pass RoboShop@1
+
+mysql -uroot -pRoboShop@1
